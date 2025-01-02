254ae931cb5e36b1d8bed80b19385da95128f585
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-02 12:42:39 -0400

    refactor evenNumbers function to use a concise arrow function syntax

--- 01-ArrowFunctions/exerc-06.js
+++ 01-ArrowFunctions/exerc-06.js
@@ -15,9 +15,7 @@ Razonamiento: "Esta pregunta está diseñada para evaluar tu habilidad para mani
 
 */
 
-function evenNumbers(numbers) {
-    return numbers.filter(num => num % 2 === 0)
-}
+const evenNumbers = numbers => numbers.filter(num => num % 2 === 0)
 
 console.log(evenNumbers([1, 2, 3, 4, 5, 6]))
 console.log(evenNumbers([10, 15, 20, 25, 30]))
