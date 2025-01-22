8bff397f76d1efd1f2542c6e0bd88b39cf08ca82
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-22 17:38:07 -0400

    fix addFive function variable naming for consistency

--- 03-map/exerc-01.js
+++ 03-map/exerc-01.js
@@ -1,7 +1,7 @@
 function addFive(numbers) {
-    const resultado = [];
-    for (let i = 0; i < numeros.length; i++) {
-        resultado.push(numeros[i] + 5);
+    const result = []
+    for (let i = 0; i < numbers.length; i++) {
+        result.push(numbers[i] + 5)
     }
-    return resultado;
+    return result
 }
