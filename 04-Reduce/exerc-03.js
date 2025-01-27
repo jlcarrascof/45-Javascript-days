633d3766359c57d25fe81b8a763a1498072a6e6d
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-27 11:27:36 -0400

    add loop to construct and log a sentence from an array of words

--- 04-Reduce/exerc-03.js
+++ 04-Reduce/exerc-03.js
@@ -1,4 +1,8 @@
 const words = ["Hello", "world", "since", "JavaScript"]
 let sentence = ""
 
+for (let i = 0; i < words.length; i++) {
+    sentence += words[i] + (i < words.length - 1 ? ", " : "")
+}
 
+console.log(sentence) // "Hello, world, since, JavaScript"
