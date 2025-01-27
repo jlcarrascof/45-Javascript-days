ad693827f6fdbb6012321dddde37e45d9354d06a
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-27 11:54:03 -0400

    add calculation of average grade in exerc-05.js

--- 04-Reduce/exerc-05.js
+++ 04-Reduce/exerc-05.js
@@ -1,2 +1,9 @@
 const grades = [85, 90, 78, 92, 88]
 let total = 0
+
+for (let i = 0; i < grades.length; i++) {
+    total += grades[i]
+}
+
+const average = total / grades.length
+console.log(average) // 86.6
