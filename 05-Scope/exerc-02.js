b4b59e48838ce67e405cd3d90a69da46b470adfd
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-02-01 20:01:36 -0400

    fix typo in variable name in exerc-02.js

--- 05-Scope/exerc-02.js
+++ 05-Scope/exerc-02.js
@@ -1,6 +1,6 @@
 function example() {
     var number = 42; // Variable declared in the function scope
-    console.log(number); // What is the value of 'numero' here?
+    console.log(number); // What is the value of 'number' here?
 }
 example();
-console.log(number); // What happens if we try to access 'numero' here?
+console.log(number); // What happens if we try to access 'number' here?
