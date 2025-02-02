f65bd5cf053f8a2e497f16cfb7ecfa106b5d1191
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-02-01 20:32:53 -0400

    add block scope example in exerc-03.js

---
+++ 05-Scope/exerc-03.js
@@ -0,0 +1,5 @@
+if (true) {
+    let name = "John"; // Variable declared in the block scope
+    console.log(name); // What is the value of 'name' here?
+}
+console.log(name); // What happens if we try to access 'name' here?
